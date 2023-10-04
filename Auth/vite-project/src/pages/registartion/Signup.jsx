ccba9e59f135b76b972571a4e67a9233c3e97fc4
codewithzaman <naderzaman50@gmail.com>
2023-10-04 21:25:03 +0500

    Update Signup.jsx
    UseState Hooks

--- Auth/vite-project/src/pages/registartion/Signup.jsx
+++ Auth/vite-project/src/pages/registartion/Signup.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Signup() {
-   
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <div className=' flex justify-center items-center h-screen'>
             <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
