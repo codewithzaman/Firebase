e008429448ad2ddf509eb8b4272ee8da62663b88
codewithzaman <naderzaman50@gmail.com>
2023-10-05 22:46:33 +0500

    Update Signup.jsx

--- Auth/vite-project/src/pages/registartion/Signup.jsx
+++ Auth/vite-project/src/pages/registartion/Signup.jsx
@@ -26,6 +26,7 @@ function Signup() {
                 <div>
                     <input type="email"
                         name='email'
+                        onChange={(e) => setEmail(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
